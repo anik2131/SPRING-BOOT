14d32944845249db92e0761f36007faf83de2bc2
anik2131 <52519854+anik2131@users.noreply.github.com>
2020-08-02 17:31:40 +0530

    Add files via upload

---
+++ ModelAttribute/src/main/java/com/anik/springboot/ModelAttributeApplication.java
@@ -0,0 +1,13 @@
+package com.anik.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelAttributeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelAttributeApplication.class, args);
+	}
+
+}
